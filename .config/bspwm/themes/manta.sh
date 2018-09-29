1aaee55bd7250a657ae258683dddb08881f16bd1
Matthew Treadwell <Treadwell@inventati.org>
2018-09-29 12:56:58 -0700

    theme updates, presel transparency

--- .config/bspwm/themes/manta.sh
+++ .config/bspwm/themes/manta.sh
@@ -1,8 +1,8 @@
 bspc config normal_border_color \#1e2541
-bspc config active_border_color \#474645
-bspc config focused_border_color \#817f7f
-bspc config presel_feedback_color \#f4d775
+bspc config active_border_color \#1e2541
+bspc config focused_border_color \#ffffff
+bspc config presel_feedback_color \#dddddd
 
 bspc config border_width         5
-bspc config window_gap          20
+bspc config window_gap          35
 
